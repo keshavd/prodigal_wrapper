52c261ef1cd2ef9bcbd961f87dea4d598c2e877c
Keshav Dial <dialk@mcmaster.ca>
2018-05-23 16:28:40 -0400

    init

---
+++ src/config.py
@@ -0,0 +1,7 @@
+
+class Config(object):
+    def __init__(self, cores=1, genus=None, files=None, output_dir=None):
+        self.cores = cores
+        self.genus = genus
+        self.files = files
+        self.output_dir = output_dir
